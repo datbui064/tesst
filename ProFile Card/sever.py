2b1477911c71089b576dd9053440931e92c1f502
DatBui1 <buiphatdat815@gmail.com>
2024-12-14 20:45:17 +0700

    first commit

---
+++ ProFile Card/sever.py
@@ -0,0 +1,12 @@
+import http.server
+import socketserver
+
+PORT = 8000
+# Địa chỉ IP của máy chủ, có thể sử dụng '0.0.0.0' để cho phép tất cả các địa chỉ IP truy cập
+HOST = '192.168.33.55'
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
+    print(f"Serving at http://{HOST}:{PORT}")
+    httpd.serve_forever()
